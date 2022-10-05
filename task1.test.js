be59cebbeb0989d19796390a3f7ff7ba11446884
IngDavidTM <david-1011@hotmail.com>
2022-10-05 13:02:21 -0500

    add function to check if the string is at least 1 character long and not longer than 10 characters

--- task1.test.js
+++ task1.test.js
@@ -1,5 +1,13 @@
 const stringLength = require('./task1.js');
 
-test ('The string has 11 characters', () => {
-    expect (stringLength('abcdefghijk')).toBe(11)
+test ('The string has 9 characters', () => {
+    expect (stringLength('abcdefghi')).toBe(9)
+})
+
+test ('The string has too many characters', () => {
+    expect(() => stringLength('abcdefghijklmn')).toThrow(Error);
+})
+
+test ('The string is too short', () => {
+    expect(() => stringLength('')).toThrow(Error);
 })
